a4a3be51e4a85776d0c0301016bc2cf0da9e18c4
Stephen F. Booth <me@sbooth.org>
2023-08-30 16:49:31 -0500

    Add ownership assertion methods

--- SFBUnfairLock.hpp
+++ SFBUnfairLock.hpp
@@ -67,6 +67,30 @@ public:
 		return os_unfair_lock_trylock(&mLock);
 	}
 
+#pragma mark Ownership
+
+	/// Asserts that the calling thread is the current owner of the lock.
+	///
+	/// If the lock is currently owned by the calling thread, this function returns.
+	///
+	/// If the lock is unlocked or owned by a different thread, this function
+	/// asserts and terminates the process.
+	inline void assert_owner() noexcept
+	{
+		os_unfair_lock_assert_owner(&mLock);
+	}
+
+	///	Asserts that the calling thread is not the current owner of the lock.
+	///
+	///	If the lock is unlocked or owned by a different thread, this function returns.
+	///
+	///	If the lock is currently owned by the current thread, this function asserts
+	///	and terminates the process.
+	inline void assert_not_owner() noexcept
+	{
+		os_unfair_lock_assert_not_owner(&mLock);
+	}
+
 private:
 
 	/// The primitive lock
