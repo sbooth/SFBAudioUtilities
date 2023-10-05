5fc432cdcc3282238337ae546d21855552c72efd
Stephen F. Booth <me@sbooth.org>
2023-10-05 11:24:53 -0500

    Add simple test

--- Tests/SFBAudioUtilitiesTests/SFBAudioUtilitiesTests.swift
+++ Tests/SFBAudioUtilitiesTests/SFBAudioUtilitiesTests.swift
@@ -2,4 +2,11 @@ import XCTest
 @testable import SFBAudioUtilities
 
 final class SFBAudioUtilitiesTests: XCTestCase {
+	func testAudioStreamBasicDescription() {
+		let asbd = AudioStreamBasicDescription(commonFormat: .float32, sampleRate: 44100, channelsPerFrame: 2, isInterleaved: false)
+		XCTAssertTrue(asbd.isFloat)
+		XCTAssertTrue(asbd.isNonInterleaved)
+		XCTAssertTrue(asbd.channelCount == 2)
+		XCTAssertTrue(asbd.channelStreamCount == 2)
+	}
 }
