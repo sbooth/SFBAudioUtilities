03c4d5b65eed87285fad36735aa095ceeda34e84
Stephen F. Booth <me@sbooth.org>
2023-10-08 11:01:48 -0500

    Add test for packetDuration

--- Tests/SFBAudioUtilitiesTests/SFBAudioUtilitiesTests.swift
+++ Tests/SFBAudioUtilitiesTests/SFBAudioUtilitiesTests.swift
@@ -6,8 +6,10 @@ final class SFBAudioUtilitiesTests: XCTestCase {
 	func testAudioStreamBasicDescription() {
 		let asbd = AudioStreamBasicDescription(commonFormat: .float32, sampleRate: 44100, channelsPerFrame: 2, isInterleaved: false)
 		XCTAssertTrue(asbd.isFloat)
+		XCTAssertTrue(!asbd.isInterleaved)
 		XCTAssertTrue(asbd.isNonInterleaved)
 		XCTAssertTrue(asbd.channelCount == 2)
 		XCTAssertTrue(asbd.channelStreamCount == 2)
+		XCTAssertTrue(asbd.packetDuration == 1/44100)
 	}
 }
