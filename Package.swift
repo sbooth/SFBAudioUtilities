6cd11771770416ef020f30fa65fa066b7d49e79d
Stephen F. Booth <me@sbooth.org>
2023-10-12 21:33:44 -0500

    Link CoreAudio.framework

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
         .target(
             name: "CXXAudioUtilities",
             dependencies: [],
-			linkerSettings: [.linkedFramework("AudioToolbox")]),
+			linkerSettings: [
+				.linkedFramework("CoreAudio"),
+				.linkedFramework("AudioToolbox"),
+			]),
 		.target(
 			name: "AVFAudioExtensions",
 			dependencies: []),
