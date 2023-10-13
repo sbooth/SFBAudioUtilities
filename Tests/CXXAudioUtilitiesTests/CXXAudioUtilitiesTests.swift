34717f4053492d14047b0f44730908e2feccd86a
Stephen F. Booth <me@sbooth.org>
2023-10-13 10:27:00 -0500

    Additional tests

--- Tests/CXXAudioUtilitiesTests/CXXAudioUtilitiesTests.swift
+++ Tests/CXXAudioUtilitiesTests/CXXAudioUtilitiesTests.swift
@@ -8,4 +8,20 @@ final class CXXAudioUtilitiesTests: XCTestCase {
 		XCTAssert(bs.Remaining() == 0)
 		XCTAssert(bs.Position() == 0)
 	}
+
+	func testStreamDescription() {
+		let asbd = SFB.CAStreamBasicDescription(.float32, 44100, 2, false)
+		XCTAssert(asbd.IsPCM() == true)
+		XCTAssert(asbd.IsFloat() == true)
+		XCTAssert(asbd.IsInteger() == false)
+		XCTAssert(asbd.mSampleRate == 44100)
+		XCTAssert(asbd.ChannelCount() == 2)
+		XCTAssert(asbd.IsInterleaved() == false)
+		XCTAssert(asbd.IsNonInterleaved() == true)
+	}
+
+	func testChannelLayout() {
+		let stereo = SFB.CAChannelLayout.Stereo
+		XCTAssert(stereo.ChannelCount() == 2)
+	}
 }
