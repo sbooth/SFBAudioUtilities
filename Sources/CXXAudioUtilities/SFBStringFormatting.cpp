1ce8f494f14d8acfcc8344a6dca11319a6007796
Stephen F. Booth <me@sbooth.org>
2024-05-01 14:42:21 -0500

    Add std::string formatting helpers

---
+++ Sources/CXXAudioUtilities/SFBStringFormatting.cpp
@@ -0,0 +1,100 @@
+//
+// Copyright (c) 2021 - 2024 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/SFBAudioUtilities
+// MIT license
+//
+
+#import <cctype>
+#import <cstdarg>
+
+#include <libkern/OSByteOrder.h>
+
+#import "SFBStringFormatting.hpp"
+
+namespace {
+
+/// Returns true if @c i consists of four printing characters
+constexpr bool fourcc_isprint(uint32_t i) noexcept
+{
+	return std::isprint(static_cast<unsigned char>(i)) && std::isprint(static_cast<unsigned char>(i >> 8)) && std::isprint(static_cast<unsigned char>(i >> 16)) && std::isprint(static_cast<unsigned char>(i >> 24));
+}
+
+/// Creates a @c std::string containing @c fourcc formatted as four characters and returns the result
+/// @throw @c std::length_error
+/// @throw @c std::bad_alloc
+/// @throw @c std::bad_array_new_length
+std::string fourcc_fourchar_string(uint32_t fourcc)
+{
+	fourcc = OSSwapHostToBigInt32(fourcc);
+	std::string s(6, '\0');
+	const auto n = std::snprintf(s.data(), 6 + 1, "'%.4s'", reinterpret_cast<const char *>(&fourcc));
+	if(n < 0)
+		return {};
+	return s;
+}
+
+/// Creates a @c std::string containing @c fourcc formatted as hexadecimal and returns the result
+/// @throw @c std::length_error
+/// @throw @c std::bad_alloc
+/// @throw @c std::bad_array_new_length
+std::string fourcc_hex_string(uint32_t fourcc)
+{
+	char buf [10 + 1];
+	const auto n = std::snprintf(buf, sizeof buf, "0x%.8x", static_cast<int>(fourcc));
+	if(n < 0)
+		return {};
+	return { buf, static_cast<std::string::size_type>(n) };
+}
+
+} // namespace
+
+std::string SFB::string_format(const char *format, ...)
+{
+	char buf [256];
+
+	// Attempt to format to `buf`
+	std::va_list args;
+	va_start(args, format);
+	const auto n = std::vsnprintf(buf, sizeof buf, format, args);
+	va_end(args);
+
+	// An error occurred
+	if(n < 0)
+		return {};
+
+	// The formatted string fit within `buf`
+	const auto len = static_cast<std::string::size_type>(n);
+	if(len < sizeof buf)
+		return { buf, len };
+
+	// Allocate a new buffer and re-run the formatting
+	std::string s(len, '\0');
+	va_start(args, format);
+	std::vsnprintf(s.data(), len + 1, format, args);
+	va_end(args);
+
+	return s;
+}
+
+std::string SFB::fourcc_string(uint32_t fourcc)
+{
+	if(fourcc_isprint(fourcc))
+		return fourcc_fourchar_string(fourcc);
+	else
+		return fourcc_hex_string(fourcc);
+}
+
+std::string SFB::osstatus_string(int32_t code)
+{
+	if(fourcc_isprint(static_cast<uint32_t>(code))) 
+		return fourcc_fourchar_string(static_cast<uint32_t>(code));
+	else if(code > -200000 && code < 200000) {
+		char buf [7 + 1];
+		const auto n = std::snprintf(buf, sizeof buf, "%d", static_cast<int>(code));
+		if(n < 0)
+			return {};
+		return { buf, static_cast<std::string::size_type>(n) };
+	}
+	else 
+		return fourcc_hex_string(static_cast<uint32_t>(code));
+}
---
+++ Sources/CXXAudioUtilities/include/SFBStringFormatting.hpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2021 - 2024 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/SFBAudioUtilities
+// MIT license
+//
+
+#pragma once
+
+#import <string>
+
+namespace SFB {
+
+/// Creates a @c std::string using a @c std::printf format string and returns the result
+/// @throw @c std::length_error
+/// @throw @c std::bad_alloc
+/// @throw @c std::bad_array_new_length
+/// @param format A null-terminated @c std::printf format string specifying how to interpret the variable arguments
+/// @return A @c std::string containing the formatted result or an empty string if a parse error occurred
+std::string string_format(const char *format, ...) __attribute__((format (printf, 1, 2)));
+
+/// Creates a string representation of a four-character code and returns the result
+/// @throw @c std::length_error
+/// @throw @c std::bad_alloc
+/// @throw @c std::bad_array_new_length
+/// @param fourcc A four-character code
+/// @return A @c std::string containing a string representation of @c fourcc
+std::string fourcc_string(uint32_t fourcc);
+
+/// Creates a string representation of an @c OSStatus result code and returns the result
+/// @throw @c std::length_error
+/// @throw @c std::bad_alloc
+/// @throw @c std::bad_array_new_length
+/// @param code An @c OSStatus result code
+/// @return A @c std::string containing a string representation of @c code
+std::string osstatus_string(int32_t code);
+
+} // namespace SFB
